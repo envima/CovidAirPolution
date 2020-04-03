939b9ab8ee54e31133ffa292efc58f1d00117236
gisma <reudenbach@uni-marburg.de>
2020-04-03 13:29:04 +0200

    add map

--- src/functions/parseHeaderWAQI.R
+++ src/functions/parseHeaderWAQI.R
@@ -4,9 +4,9 @@ parseHeaderWAQI = function(header, getLatLon = TRUE){
   
   if(getLatLon){
     info = stringr::str_extract_all(string = str_subset(header, "latitude"), pattern = "\\d+\\.*\\d*")[[1]]
-    latlon = data.frame(Latitude = as.numeric(info[1]),
-               Longitude = as.numeric(info[2]))
+    # latlon = data.frame(Latitude = as.numeric(info[1]),
+    #            Longitude = as.numeric(info[2]))
   }
 
-  return(list(latlon = latlon))  
+  return(info)  
 }
