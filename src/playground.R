3aabc72f81c03371c5dcaec6d36110bf53eee9cc
gisma <reudenbach@uni-marburg.de>
2020-04-05 11:14:41 +0200

    add wavelet

--- src/playground.R
+++ src/playground.R
@@ -1,6 +1,8 @@
 # Set up working environment and defaults --------------------------------------
 library(envimaR)
 library(htmlTable)
+library(wavelets)
+library(WaveletComp)
 root_folder = path.expand("~/project/cov/CovidAirPolution/")
 source(file.path(root_folder, "src/functions/000_setup.R"))
 source(file.path(root_folder, "src/functions/parseHeaderWAQI.R"))
@@ -18,13 +20,20 @@ waqnames <- getWAQINames(flist)
 act$statname<-substr(basename(f), regexec("\\.", basename(f))[[1]][1]+1, regexec("--", basename(f))[[1]][1]-1)
 # create mapview data
 pm25<-lapply(aq, function(x) x[,1:4])
-for (i in 1:length(pm25))names(pm25[[i]])<-c("date","pm25","pm25min","pm25max")
+for (i in 1:length(pm25))names(pm25[[i]])<-c("data","pm25","pm25min","pm25max")
 pop = lapply(pm25, htmlTable)
 mapview(pts, popup = pop,legend = FALSE, ncol="pm25max")
 
 res<-getCovidIT()
 cov_admin2<-res[[1]]
 cov_admin3 <-res[[2]]
+
+
+
+
+
+#---------------------------------------------------------
+# plot data with plotly
 
 # ggplot() + 
 #   geom_line(data = cmb, aes(x = date, y = pm25, colour = stat)) + 
@@ -49,7 +58,73 @@ fig = fig %>% add_trace(data = cov_admin3[cov_admin3$denominazione_provincia == 
 fig = fig %>% layout(yaxis2 = list(overlaying = "y", side = "right", title = "new cases"))
 fig
 
-#-----tidy time lag
+
+
+#---------------------------------------------------
+# add waveletComp example for bergamo
+
+# merge data colums
+bergamo<-merge(x = cov_admin3[cov_admin3$denominazione_provincia == "Bergamo",], y = pm25[[8]], by = "data",sort = T)
+head(bergamo)
+plot(bergamo$new_cases,type = "l")
+plot(bergamo$pm25,type = "l")
+
+# convert date to factor
+bergamo$data<-as.factor(as.character(bergamo$data))
+
+# new_cases only is there a periodicy?
+
+bergamo.w <- analyze.wavelet(bergamo, "new_cases",loess.span = 0,dt = 1, dj = 1/50,lowerPeriod = 1, upperPeriod = 28,make.pval = TRUE, n.sim = 10)
+
+wt.image(bergamo.w, color.key = "interval", n.levels = 250,legend.params = list(lab = "wavelet power levels"),periodlab = "period (days)", timelab = "")
+wt.image(bergamo.w, color.key = "interval", n.levels = 250,legend.params = list(lab = "wavelet power levels"),periodlab = "period (days)",label.time.axis = TRUE)
+
+# - change scale 
+max.power <- max(bergamo.w$Power)
+
+wt.image(bergamo.w, color.key = "interval", n.levels = 250,legend.params = list(lab = "wavelet power levels"),periodlab = "period (days)",maximum.level = 1.001*max.power,show.date = TRUE, date.format = "%F", timelab = "")
+wt.image(bergamo.w, color.key = "interval", n.levels = 250,legend.params = list(lab = "wavelet power levels",
+                                                                           label.digits = 2),
+           periodlab = "periods (days)", maximum.level = 1.25, 
+         # Concerning item 1 above --- plot the square root of power:
+         exponent = 0.5,
+         # Concerning item 2 above --- time axis:
+         show.date = FALSE, date.format = "%m%d", timelab = "", spec.time.axis = list(at = c(paste(2005:2014, "-01-01", sep = "")),labels = c(2005:2014)),timetcl = -0.5,  
+         # draws outward ticks# Concerning item 3 above --- period axis:
+         spec.period.axis = list(at = c(3, 7, 14, 21, 28)),periodtck = 1, periodtcl = NULL  )
+         # draws horizontal lines)
+
+#--------------------------------------------------------------------
+# cross wavelet anaklysis
+
+bergamo.wc <- analyze.coherency(bergamo,my.pair = c("new_cases", "pm25"),
+                                loess.span = 0,dt = 1, dj = 1/50,lowerPeriod = 1, upperPeriod = 28,
+                                make.pval = TRUE, n.sim = 10)
+
+max.power <- max(bergamo.wc$Power.xy)  # for plotting
+#save(bergamo.wc, file = "cross_wavelet_transform_temperature_over_humidity")
+
+#load("cross_wavelet_transform_temperature_over_humidity")
+exponent <- 0.5
+wc.image(bergamo.wc, n.levels = 250,
+         legend.params = list(lab = "cross-wavelet power levels"),
+         color.key = "interval",
+         maximum.level = (1.001*max.power)^exponent, exponent = exponent,
+         # time axis:
+         label.time.axis = TRUE, 
+         show.date = TRUE,
+         spec.time.axis = list(at = paste(2020-02-01:2020-04-02, "-01-01", sep = ""),labels = 2020-02:2020-04),timetcl = -0.5, 
+         # outward ticks
+         # period axis:
+         periodlab = "period (days)",
+         spec.period.axis = list(at = c(3, 7, 14, 21, 28)),periodtck = 1, periodtcl = NULL)
+
+
+
+
+
+#---------------------------------------------------------
+# timesereis tidy lags
 
 
 
@@ -111,6 +186,10 @@ tidyverse_count_autocorrelations %>%
     axis.text.x = element_text(angle = 45, hjust = 1)
   )
 ####---------------
+
+
+
+
 
 # Get the absolute autocorrelations
 tidyverse_absolute_autocorrelations <- tidyverse_count_autocorrelations %>%
