6f60def0ccea61f98b8f1b5edf4dd0426b81f848
Thomas Nauss <nauss@staff.uni-marburg.de>
2020-04-05 16:46:44 +0200

    Combine air quality per province

--- src/functions/makeSFPointsWAQI.R
+++ src/functions/makeSFPointsWAQI.R
@@ -5,11 +5,12 @@ makeSFPointsWAQI = function(pm_waqi){
     latlon = c(x[1, "lon"],x[1, "lat"])
     class(latlon) = "numeric"
     p = st_sfc(st_point(latlon),crs = 4326)
-    st_sf(info = x[1, "statname"],p)
+    st_sf(aq_location = x[1, "statname"],p)
   })
   pts = do.call(rbind, p)
   
   pop = lapply(lapply(pm_waqi, function(x) x[ ,c("date","pm25","pm25_min","pm25_max")]), htmlTable)
+  names(pop) = NULL
   
   return(list(pts = pts, pop = pop))
 }
