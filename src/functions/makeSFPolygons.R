52da7a3de800de725c2d89cf862f4bd2d4b91440
Thomas Nauss <nauss@staff.uni-marburg.de>
2020-04-09 23:50:50 +0200

    Bug fix in German data and empty values.

--- src/functions/makeSFPolygons.R
+++ src/functions/makeSFPolygons.R
@@ -1,5 +1,6 @@
 # Compile SF point vector dataset
 
+# Data for Italy ---------------------------------------------------------------
 makeSFPolygonsIT = function(nuts){
   p = lapply(unique(nuts$codice_provincia), function(p){
     act = nuts[nuts$codice_provincia == p, ] 
@@ -14,18 +15,13 @@ makeSFPolygonsIT = function(nuts){
 }
 
 
-makeSFPolygonsDE = function(nuts){
+# Data for Germany -------------------------------------------------------------
+makeSFPolygonsDE = function(covdata){
   
   rtyp3 = st_read(file.path(envrmt$path_data, "landkreise-in-germany/landkreise-in-germany.shp"))
   rtyp3$cca_2 = as.numeric(as.character(rtyp3$cca_2))
-  rtyp3 = merge(rtyp3, nuts, by.x = "cca_2", by.y = "nuts3")
-  # 
-  # p = lapply(unique(nuts$nuts3), function(p){
-  #   act = nuts[nuts$nuts3 == p, ]
-  #   act = merge(rtyp3, act, by.x = "cca_2", by.y = "nuts3")
-  # })
-  # plgns = do.call(rbind, p)
-  # it  = ne_states(country = "italy", returnclass = "sf")
-  # it = it[, "adm1_code"]
+  rtyp3 = merge(rtyp3, covdata, by.x = "cca_2", by.y = "nuts3Code")
+  names(rtyp3)[1] = "nuts3Code"
+  rtyp3 = rtyp3[, c(names(covdata), "name_2")]
   return(rtyp3)
 }
--- src/functions/makedfUBA.R
+++ src/functions/makedfUBA.R
@@ -3,24 +3,49 @@
 makedfUBA = function(flist){
   
   geo = read.table(file.path(envrmt$path_DE, "station_All_convert.csv"), skip = 1, header = TRUE, sep = ";", dec = ",")
-  geo = geo[, c("Stationscode", "Stationsname", "Länge.dezimal", "Breite.dezimal")]
-  geo$Stationscode = as.character(geo$Stationscode)
+  geo = geo[, c(1, 2, 13, 14)]
+  names(geo) = c("stationcode", "stationname", "lon", "lat")
+  geo$stationcode = as.character(geo$stationcode)
   
   uba = read.table(flist, skip = 0, header = TRUE, sep = ";")
   uba$date = as.POSIXct(as.character(uba$Datum), format = "%Y%m%d", origin = "MEZ")
-  uba$pm25 = rowMeans(uba[, c(which(colnames(uba) == "Wert01") : which(colnames(uba) == "Wert24"))])
-  uba$Station = as.character(uba$Station)
-  uba = merge(uba, geo, by.x = "Station", by.y = "Stationscode")
-  uba$lat = uba$Breite.dezimal
-  uba$lon = uba[, "Länge.dezimal"]
-  uba = uba[, -c(which(names(uba) == "Länge.dezimal"), which(names(uba) == "Breite.dezimal"))]
   
-  aq = lapply(unique(uba$Station), function(s) {
-    act = uba[uba$Station == s, ]
+  # Aggregate hourly values and set -999 to NA
+  ch01 = which(colnames(uba) == "Wert01")
+  ch24 = which(colnames(uba) == "Wert24")
+  uba[, ch01:ch24][uba[, ch01:ch24] < 0] = NA
+  uba$pm25 = rowMeans(uba[, ch01:ch24], na.rm = TRUE)
+  uba$Station = as.character(uba$Station)
+  uba = uba[, c(1, 32, 33, 4:5 )]
+  names(uba)[1:3] = c("stationcode", "date", "pm25")
+  
+  uba = merge(uba, geo, by = "stationcode")
+  uba$lat = uba$lat
+  uba$lon = uba$lon
+  
+  # Move each station to individual list and fill NAs
+  aq = lapply(unique(uba$stationcode), function(s) {
+    act = uba[uba$stationcode == s, ]
+    
+    lna = which(is.na(act$pm25))
+    filled = na.approx(act$pm25, na.rm = FALSE)
+    
+    if(!is_empty(lna)){
+      if(lna[1] == 1){
+        first_valid = which(!is.na(filled))[1]
+        filled[1:(first_valid - 1)] = filled[first_valid]
+      }
+      
+      if(lna[length(lna)] == length(filled)){
+        last_valid = tail(which(!is.na(filled)), 1)
+        filled[(last_valid + 1):length(filled)] = filled[last_valid]
+      }
+    } 
+    
     return(act)
   })
   
-  names(aq) = sapply(aq, function(x) x[1, "Stationsname"])
+  names(aq) = sapply(aq, function(s) s[1, "stationname"])
   
   return(aq)
   
