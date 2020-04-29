a3d8e09b67036b2bcb23285456f1ff49c9a65cf0
Thomas Nauss <nauss@staff.uni-marburg.de>
2020-04-29 15:48:27 +0200

    Add Italy

--- src/functions/compileDataIT.R
+++ src/functions/compileDataIT.R
@@ -16,17 +16,31 @@ compileDataIT = function(){
   
   # Merge air quality and COVID data -------------------------------------------
   it_nuts3 = st_join(cov_it_polygons, pm_waqi_points$pts)
-  it_nuts3 = it_nuts3[!is.na(it_nuts3$aq_location), ]
+  it_nuts3 = it_nuts3[!is.na(it_nuts3$stationname), ]
   
-  it_nuts3 = lapply(unique(it_nuts3$aq_location), function(l){
+  it_nuts3 = lapply(unique(it_nuts3$stationname), function(l){
+    print(l)
     m = merge(it_nuts3,  pm_waqi[[as.character(l)]], 
-              by.x = c("aq_location", "date"), by.y = c("statname", "date"))
+              by.x = c("stationname", "date"), by.y = c("stationname", "date"), all.y = TRUE)
+    
     cn = names(it_nuts3)
     cn[cn=="lat"] = "lat.x"
     cn[cn=="lon"] = "lon.x"
     
+    fill_pos = which(!is.na(m$denominazione_provincia))[1]
+    
+    m$adm1_code = m$adm1_code[fill_pos]
+    m$stato = m$stato[fill_pos]
+    m$codice_regione = m$codice_regione[fill_pos]
+    m$denominazione_regione = m$denominazione_regione[fill_pos]
+    m$sigla_provincia = m$sigla_provincia[fill_pos]
+    m$denominazione_provincia = m$denominazione_provincia[fill_pos]
+    m$lat.x = m$lat.x[fill_pos]
+    m$lon.x = m$lon.x[fill_pos]
+    m$totale_casi[is.na(m$totale_casi)] = 0
+    
     m = m[, c(cn, 
-              "pm25", "pm25_min", "pm25_max",
+              "pm25",
               "lat.y", "lon.y")]
   })
   
@@ -37,12 +51,16 @@ compileDataIT = function(){
   it_nuts3_mean = lapply(nuts3_names, function(p){
     act = it_nuts3[it_nuts3$denominazione_provincia == p,]
     
-    pm = aggregate(list(act$pm25, act$pm25_min, act$pm25_max), 
+    pm = aggregate(list(act$pm25), 
                    by = list(act$date), FUN = mean, na.rm=TRUE)
-    names(pm) = c("date", "pm25_mean", "pm25_min_mean", "pm25_max_mean")
+    names(pm) = c("date", "pm25_mean")
+    
+    u_stations = unique(act$stationname)
+    
+    new_stationname = paste(length(u_stations), u_stations, collapse = "-")
     
     act = merge(
-      pm, act[seq(nrow(act[act$aq_location == unique(act$aq_location)[1],])), ], 
+      pm, act[seq(nrow(act[act$stationname == u_stations[1],])), ],
       by.x = "date", by.y = "date")
     
     act = st_set_geometry(act, act$geometry)
