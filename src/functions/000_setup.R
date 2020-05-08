84c78d4f2b6ca94ad3fd07813cf39aa3ab6ed80e
Thomas Nauss <nauss@staff.uni-marburg.de>
2020-05-08 18:04:16 +0200

    Update figures.

--- src/functions/000_setup.R
+++ src/functions/000_setup.R
@@ -23,7 +23,7 @@ project_folders = c("data/",
 libs = c("doParallel", "caret", "data.table", "dtwclust", "ggplot2", "htmlTable", "htmltools", 
          "jsonlite", "plotly", "mapview", "stringr", "sf",
          "sf", "wavelets", "wmtsa", "mgcv", "biwavelet", "rnaturalearth", 
-         "rnaturalearthhires", "tidygeocoder", "ggspatial",
+         "rnaturalearthhires", "tidygeocoder", "ggspatial", "cowplot",
          "tidyverse","tidyquant","timetk","forcats","tidyr", "WaveletComp")
 
 envrmt = createEnvi(root_folder = root_folder,
