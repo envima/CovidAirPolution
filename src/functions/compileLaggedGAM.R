fa3254c9be16f17c0a3ff1aebd28acbdd52e12b2
Thomas Nauss <nauss@staff.uni-marburg.de>
2020-05-16 16:15:36 +0200

    Some more testing

--- src/functions/compileLaggedGAM.R
+++ src/functions/compileLaggedGAM.R
@@ -1,71 +1,53 @@
 #' Compile lagged gam models.
 
-compileLaggedGAM = function(data){
-  model_lag = lapply(unique(data$nuts3Code), function(n){
-    
-    model_lag = lapply(seq(0, 14), function(l){
-      
-      tmp = data[data$nuts3Code == n, ]
-      
-      tmp = tmp %>%
-        # group_by(nuts3Code) %>%
-        mutate(pm_mean_lag = dplyr::lag(pm_mean, n = l, default = NA))
-      
-      # tmp = tmp[!is.na(tmp$cases_lag), ]
-      # print(tmp$pm_mean_lag)
-      
-      tmp_gam = gam(new_cases ~ s(seq(length(date))) + pm_mean_lag, family = quasipoisson(link = "log"), data = tmp)
-      
-      test = summary(tmp_gam)
-      data.frame(nuts3_code = n,
-                 cluster = tmp$cluster_covid[1],
-                 cluster_pm = tmp$cluster_pm[1],
-                 pm_mean_mean = mean(tmp$pm_mean),
-                 lag = -l, 
-                 t = test$p.t["pm_mean_lag"],
-                 p = test$p.pv["pm_mean_lag"])
-    })
-    model_lag = do.call("rbind", model_lag)
-    
-  })
-  model_lag = do.call("rbind", model_lag)
-  return(model_lag)
-}
-
-
-
-compileLaggedGAMboth = function(data){
+compileLaggedGAM = function(data, target = "new_cases", nlags = 14,
+         min_cases = 1, individual = FALSE, ndays = c(-14, 32),
+         obsprd_start = NULL, obsprd_end = NULL){
+  
   
   model_lag = lapply(unique(data$nuts3Code), function(n){
     
-    model_lag = lapply(seq(0, 14), function(l){
+    if(individual){
+      obsprd_start = data$date[which(data[data$nuts3Code == n, "cases"] >= min_cases)[1] + ndays[1]]
+      obsprd_end = data$date[which(data[data$nuts3Code == n, "cases"] >= min_cases)[1] + ndays[2]]
+    } else {
+      obsprd_start = as.POSIXct("2020-02-15")
+      obsprd_end =  as.POSIXct("2020-04-01")
       
-      tmp = data[data$nuts3Code == n, ]
+    }
+    
+    if(obsprd_end > obsprd_start){
       
-      tmp25 = tmp %>%
-        mutate(pm25_mean_lag = dplyr::lag(pm25_mean, n = l, default = NA))
-      
-      tmp10 = tmp %>%
-        mutate(pm10_mean_lag = dplyr::lag(pm10_mean, n = l, default = NA))
-      
-      tmp = merge(tmp25, tmp10)
-      
-      tmp_gam = gam(cases ~ date + weekday_c + pm10_mean_lag + pm25_mean_lag, family = quasipoisson, data = tmp)
-      
-      test = summary(tmp_gam)
-      data.frame(nuts3_code = n,
-                 cluster = tmp$cluster_covid[1],
-                 cluster_pm = tmp$cluster_pm[1],
-                 pm_mean_mean = mean(tmp$pm_mean),
-                 lag = -l, 
-                 t25 = test$coefficients["pm25_mean_lag", "t value"], 
-                 p25 = test$coefficients["pm25_mean_lag", "Pr(>|t|)"],
-                 t10 = test$coefficients["pm10_mean_lag", "t value"], 
-                 p10 = test$coefficients["pm10_mean_lag", "Pr(>|t|)"])
-    })
-    model_lag = do.call("rbind", model_lag)
+      model_lag = lapply(seq(0, nlags), function(l){
+        
+        tmp = data[data$nuts3Code == n, ]
+        
+        tmp = tmp %>%
+          mutate(pm_mean_lag = dplyr::lag(pm_mean, n = (l), default = NA))
+        
+        frml = as.formula(paste(target, "~ s(seq(length(date)), bs='tp', sp=0.6) + weekday_cn + pop_dens + pm_mean_lag"))
+        set.seed(01042020)
+        tmp_gam = gam(frml, family = quasipoisson(link = "log"), data = tmp[tmp$date >= obsprd_start & tmp$date <= obsprd_end,])
+        
+        test = summary(tmp_gam)
+        data.frame(nuts3_code = n,
+                   cluster = tmp$cluster_covid[1],
+                   cluster_pm = tmp$cluster_pm[1],
+                   pm_mean_mean = mean(tmp$pm_mean),
+                   lag = -l, 
+                   t = test$p.t["pm_mean_lag"],
+                   p = test$p.pv["pm_mean_lag"],
+                   obsprd_start = obsprd_start,
+                   obsprd_end = obsprd_end)
+      })
+      model_lag = do.call("rbind", model_lag)
+    } else {
+      model_lag = NULL
+    }
     
   })
   model_lag = do.call("rbind", model_lag)
+  
+  
   return(model_lag)
 }
--- src/functions/compileLaggedGLM.R
+++ src/functions/compileLaggedGLM.R
@@ -1,12 +1,12 @@
 #' Compile lagged glm models.
 
-compileLaggedGLM = function(data, target = "new_cases", nlags = 14,
+compileLaggedGLM = function(data, pm = "org", frml, nlags = 14,
                             min_cases = 1, individual = FALSE, ndays = c(-14, 32),
                             obsprd_start = NULL, obsprd_end = NULL){
   
   
   model_lag = lapply(unique(data$nuts3Code), function(n){
-    
+
     if(individual){
       obsprd_start = data$date[which(data[data$nuts3Code == n, "cases"] >= min_cases)[1] + ndays[1]]
       obsprd_end = data$date[which(data[data$nuts3Code == n, "cases"] >= min_cases)[1] + ndays[2]]
@@ -16,31 +16,46 @@ compileLaggedGLM = function(data, target = "new_cases", nlags = 14,
       
     }
     
-    model_lag = lapply(seq(0, nlags), function(l){
+    if(obsprd_end > obsprd_start){
       
-      tmp = data[data$nuts3Code == n, ]
-      
-      tmp = tmp %>%
-        mutate(pm_mean_lag = dplyr::lag(pm_mean_dt, n = (l), default = NA))
-      
-      frml = as.formula(paste(target, "~ date + weekday_c + pm_mean_lag"))      
-      tmp_glm = glm(frml , family = quasipoisson, data = tmp[tmp$date >= obsprd_start & tmp$date <= obsprd_end,])
-      
-      test = summary(tmp_glm)
-      data.frame(nuts3_code = n,
-                 cluster = tmp$cluster_covid[1],
-                 cluster_pm = tmp$cluster_pm[1],
-                 pm_mean_mean = mean(tmp$pm_mean),
-                 lag = -l, 
-                 t = test$coefficients["pm_mean_lag", "t value"], 
-                 p = test$coefficients["pm_mean_lag", "Pr(>|t|)"],
-                 obsprd_start = obsprd_start,
-                 obsprd_end = obsprd_end)
-    })
-    model_lag = do.call("rbind", model_lag)
+      model_lag = lapply(seq(0, nlags), function(l){
+        
+        tmp = data[data$nuts3Code == n, ]
+        
+        if(pm == "org"){
+          tmp = tmp %>%
+            mutate(pm_mean_lag = dplyr::lag(pm_mean, n = (l), default = NA))
+        } else {
+          tmp = tmp %>%
+            mutate(pm_mean_lag = dplyr::lag(pm_mean_dt, n = (l), default = NA))
+        }
+        
+        frml = as.formula(frml)      
+        set.seed(01042020)
+        tmp_glm = glm(frml , family = quasipoisson, data = tmp[tmp$date >= obsprd_start & tmp$date <= obsprd_end,])
+        # tmp_glm = zeroinfl(new_cases ~ date_numeric + weekday_c + pm_mean_lag | days_before_shutdown, data = tmp[tmp$date >= obsprd_start & tmp$date <= obsprd_end,])
+        
+        test = summary(tmp_glm)
+        data.frame(nuts3_code = n,
+                   cluster = tmp$cluster_covid[1],
+                   cluster_pm = tmp$cluster_pm[1],
+                   pm_mean_mean = mean(tmp$pm_mean),
+                   lag = -l, 
+                   # t = test$coefficients["pm_mean_lag", "t value"], 
+                   # p = test$coefficients["pm_mean_lag", "Pr(>|t|)"],
+                   t = test$coefficients$count["pm_mean_lag", "z value"], 
+                   p = test$coefficients$count["pm_mean_lag", "Pr(>|z|)"],
+                   obsprd_start = obsprd_start,
+                   obsprd_end = obsprd_end)
+      })
+      model_lag = do.call("rbind", model_lag)
+    } else {
+      model_lag = NULL
+    }
     
   })
   model_lag = do.call("rbind", model_lag)
+  
   
   return(model_lag)
 }
